d374f95a5d4ca9c698733f531eb2414a232d43fb
Ruchika Shivaswamy <ruchikas300@gmail.com>
2016-07-08 15:14:48 -0700

    updated antarin upload command

--- antarin-cli/antarin/commands/upload.py
+++ antarin-cli/antarin/commands/upload.py
@@ -5,18 +5,16 @@ import json, urllib2, urllib,os
 import requests
 
 class Upload(Base):
-	"""docstring for Upload"""
 	def send_request(self,token):
 		filename = json.loads(json.dumps(self.options))['<file>']
-		url = "http://webapp-test.us-west-2.elasticbeanstalk.com/rest-fileupload/" + filename
-		#val = {"token":token}
+		url = "http://webapp-test.us-west-2.elasticbeanstalk.com/rest-fileupload/" + filename			
 		files = {
 			'token' : (None, json.dumps(token), 'application/json'),
          	'file': (os.path.basename(filename), open(filename, 'rb'), 'application/octet-stream')
 		}
+		#print files
 		try:
 			connection = requests.put(url, files=files)
-			#print files
 		except e:
 			connection = e
 		return connection
@@ -27,13 +25,12 @@ class Upload(Base):
 		token = config.get('user_details', 'token')
 		if token != "":
 			connection = Upload.send_request(self,token)
-			print "File upload successful"
-			#print connection
-			# if connection.code == 204:
-			# 	data = connection.read()
-			# 	print data
-			# else:
-			# 	print 'Error while fetching files'
+			#print "File upload successful"
+			#print connection.status_code
+			if connection.status_code == 204:
+				print "File upload successful"
+			else:
+				print 'Error while fetching files'
 		else:
 			print "Looks like you have not verified your login credentials yet.Please try this command after authetication is compelete."
 

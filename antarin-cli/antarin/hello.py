107077484554b0d717e55cfa5ddfeb3da94bf314
Kevin YB <kevin.yedid@gmail.com>
2016-07-20 21:11:02 -0700

    adding antarin API upload example

--- antarin-cli/antarin/hello.py
+++
@@ -1,8 +0,0 @@
-class Hello(object):
- 	"""docstring for Hello"""
-	def __init__(self, message):
- 		self.message= message
-
- 	def display_message(self):
- 		 print 'Hello, ' + self.message + '!'
- 		

f9ded0be1840fdbab293fda8ee16fe090f404781
Kevin YedidBotton <kevin.yedid@gmail.com>
2016-07-28 11:33:45 -0700

    deleting webapp from origin master

---
+++ antarin-cli/antarin/commands/docs.py
@@ -0,0 +1,87 @@
+
+
+
+
+"""
+antarinX
+
+Usage:
+  ax login
+  ax summary
+  ax -h | --help *** 
+  ax ls
+  ax pwd
+  ax cd [<foldername>]
+  ax rm [-r] <folder/file>
+  ax mkdir <foldername>
+  ax upload <file>
+  ax --version
+  ax logout
+  ax env
+  ax newproject <projectname>
+  ax listprojects
+  ax enterproject <projectname>
+  ax importdata --file <filename>
+  ax importdata --folder <foldername>
+  ax addcontributor <username>
+  ax exitproject
+  ax help
+
+"""
+
+
+
+Usage: 
+	
+	Filesystem Tools: 
+
+		To login into your account: 
+		$ ax login 
+
+		$ ax summary 
+
+		To list all your files: 
+		$ ax ls
+
+		$ ax pwd 
+
+		$ ax cd [<foldername>]
+
+		$ ax rm [-r] <folder/file>
+
+		$ ax mkdir <foldername>
+
+		$ ax upload <folder/file>
+
+		$ ax logout 
+
+
+	Project Tools: 
+
+		$ ax newproject <projectname>
+
+		$ ax listprojects
+
+		$ ax enterproject <projectname>
+
+		$ ax importdata --file <filename>
+
+		$ ax importdata --folder <foldername>
+
+		$ ax addcontributor <username>
+
+		$ ax summary 
+
+		$ ax ls
+
+		$ ax cd [<foldername>]
+
+		$ ax mkdir <foldername>
+
+		$ ax rm [-r] <folder/file>
+
+		$ ax exitproject
+
+
+
+

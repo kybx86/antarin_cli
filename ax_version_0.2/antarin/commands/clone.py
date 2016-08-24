dda11f4c27ef3379fe882a36d32eb08d6872aa78
Ruchika Shivaswamy <ruchikas300@gmail.com>
2016-08-24 10:31:26 -0700

    adding clone command

---
+++ ax_version_0.2/antarin/commands/clone.py
@@ -0,0 +1,27 @@
+"""
+	ax clone <id>
+"""
+
+from .base import Base
+from ..utils import apicalls,iocalls
+
+
+class Clone(Base):
+
+	def response_handler(self,payload):
+		print(payload[1])
+
+	def run(self):
+		config = self.config
+		self.endpoint = '/clone/'
+
+		if config.auth():
+			if self.config.space_env():
+				accesskey = self.option_dict['<id>']
+				if accesskey.isdigit():
+					payload = self.send_request(self.endpoint,None,accesskey)
+					self.response_handler(payload)
+				else:
+					iocalls.print_not_num_text()
+			else:
+				iocalls.print_not_valid_argument()
---
+++ ax_version_0.2/antarin/commands/merge.py
@@ -0,0 +1,27 @@
+"""
+	ax merge <source_id> <destination_id>
+"""
+
+from .base import Base
+from ..utils import apicalls,iocalls
+
+class Merge(Base):
+
+	def response_handler(self,payload):
+		print(payload[1])
+		
+	def run(self):
+		config = self.config
+		self.endpoint = '/merge/'
+
+		if config.auth():
+			if self.config.space_env():
+				source_id = self.option_dict['<source_id>']
+				destination_id = self.option_dict['<destination_id>']
+				if source_id.isdigit() and destination_id.isdigit():
+					payload = self.send_request(self.endpoint,None,None,None,None,None,None,None,source_id,destination_id)
+					self.response_handler(payload)
+				else:
+					iocalls.print_not_num_text()
+			else:
+				iocalls.print_not_valid_argument()
